0229b48df57eb4712ee461ed1ef63b6c0f68ff69
rakaarfi <rakaarfi@gmail.com>
2025-01-28 17:32:49 +0700

    add new router

--- src/infrastructure/api/ics_206/ics_206_router.py
+++ src/infrastructure/api/ics_206/ics_206_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.infrastructure.api.ics_206 import (
                                 ics_206, ics_206_preparation,
                                 hospitals, medical_aid_station,
-                                transportation)
+                                transportation, ics_206_approval)
 
 router = APIRouter()
 
@@ -12,3 +12,4 @@ router.include_router(hospitals.router, prefix="/hospitals")
 router.include_router(medical_aid_station.router, prefix="/medical-aid-station")
 router.include_router(transportation.router, prefix="/transportation")
 router.include_router(ics_206_preparation.router, prefix="/preparation")
+router.include_router(ics_206_approval.router, prefix="/approval")
