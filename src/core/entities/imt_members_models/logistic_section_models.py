f922a82bc4d1aef2f2db7af3d89cc84aa75e5d52
rakaarfi <rakaarfi@gmail.com>
2025-01-28 17:56:58 +0700

    change table name

--- src/core/entities/imt_members_models/logistic_section_models.py
+++ src/core/entities/imt_members_models/logistic_section_models.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlmodel import Relationship
 
 from src.core.entities.base_imt_models import BaseImtMember
-from src.core.entities.ics_201_models import IcsChart
+from src.core.entities.ics_201_models import Ics201Chart
 from src.core.entities.ics_203_models import Ics203
 from src.core.entities.ics_205_models import Ics205Preparation
 from src.core.entities.ics_206_models import Ics206Preparation
@@ -13,7 +13,7 @@ from src.core.entities.roster_models import ImtRoster
 class LogisticSectionChief(BaseImtMember, table=True):
     __tablename__ = "logistic_section_chief"
     imt_rosters: List[ImtRoster] = Relationship(back_populates="logistic_section_chief")
-    ics_chart: List[IcsChart] = Relationship(back_populates="logistic_section_chief")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="logistic_section_chief")
     ics_203: List[Ics203] = Relationship(back_populates="logistic_section_chief")
 
 
@@ -22,7 +22,7 @@ class CommunicationUnitLeader(BaseImtMember, table=True):
     imt_rosters: List[ImtRoster] = Relationship(
         back_populates="communication_unit_leader"
     )
-    ics_chart: List[IcsChart] = Relationship(back_populates="communication_unit_leader")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="communication_unit_leader")
     ics_203: List[Ics203] = Relationship(back_populates="communication_unit_leader")
     ics_205_preparation: List[Ics205Preparation] = Relationship(
         back_populates="communication_unit_leader"
@@ -32,7 +32,7 @@ class CommunicationUnitLeader(BaseImtMember, table=True):
 class MedicalUnitLeader(BaseImtMember, table=True):
     __tablename__ = "medical_unit_leader"
     imt_rosters: List[ImtRoster] = Relationship(back_populates="medical_unit_leader")
-    ics_chart: List[IcsChart] = Relationship(back_populates="medical_unit_leader")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="medical_unit_leader")
     ics_203: List[Ics203] = Relationship(back_populates="medical_unit_leader")
     ics_206_preparation: List[Ics206Preparation] = Relationship(
         back_populates="medical_unit_leader"
@@ -42,21 +42,21 @@ class MedicalUnitLeader(BaseImtMember, table=True):
 class FoodUnitLeader(BaseImtMember, table=True):
     __tablename__ = "food_unit_leader"
     imt_rosters: List[ImtRoster] = Relationship(back_populates="food_unit_leader")
-    ics_chart: List[IcsChart] = Relationship(back_populates="food_unit_leader")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="food_unit_leader")
     ics_203: List[Ics203] = Relationship(back_populates="food_unit_leader")
 
 
 class FacilityUnitLeader(BaseImtMember, table=True):
     __tablename__ = "facility_unit_leader"
     imt_rosters: List[ImtRoster] = Relationship(back_populates="facility_unit_leader")
-    ics_chart: List[IcsChart] = Relationship(back_populates="facility_unit_leader")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="facility_unit_leader")
     ics_203: List[Ics203] = Relationship(back_populates="facility_unit_leader")
 
 
 class SupplyUnitLeader(BaseImtMember, table=True):
     __tablename__ = "supply_unit_leader"
     imt_rosters: List[ImtRoster] = Relationship(back_populates="supply_unit_leader")
-    ics_chart: List[IcsChart] = Relationship(back_populates="supply_unit_leader")
+    ics_201_chart: List[Ics201Chart] = Relationship(back_populates="supply_unit_leader")
     ics_203: List[Ics203] = Relationship(back_populates="supply_unit_leader")
 
 
@@ -65,7 +65,7 @@ class TransportationUnitLeader(BaseImtMember, table=True):
     imt_rosters: List[ImtRoster] = Relationship(
         back_populates="transportation_unit_leader"
     )
-    ics_chart: List[IcsChart] = Relationship(
+    ics_201_chart: List[Ics201Chart] = Relationship(
         back_populates="transportation_unit_leader"
     )
     ics_203: List[Ics203] = Relationship(back_populates="transportation_unit_leader")
