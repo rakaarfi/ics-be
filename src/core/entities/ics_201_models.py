55b8d5d4614be649e3cf408daca552408b22bcd1
rakaarfi <rakaarfi@gmail.com>
2025-01-28 17:55:37 +0700

    change table name

--- src/core/entities/ics_201_models.py
+++ src/core/entities/ics_201_models.py
@@ -16,17 +16,17 @@ class Ics201(SQLModel, table=True):
     situation_summary: str
     objectives: str
 
-    actions_strategies_tactics: List["ActionsStrategiesTactics"] = Relationship(
+    ics_201_actions_strategies_tactics: List["Ics201ActionsStrategiesTactics"] = Relationship(
         back_populates="ics_201"
     )
-    resource_summary: List["ResourceSummary"] = Relationship(back_populates="ics_201")
-    ics_chart: "IcsChart" = Relationship(back_populates="ics_201")
+    ics_201_resource_summary: List["Ics201ResourceSummary"] = Relationship(back_populates="ics_201")
+    ics_201_chart: "Ics201Chart" = Relationship(back_populates="ics_201")
 
     incident_data: Optional["IncidentData"] = Relationship(back_populates="ics_201")
 
 
-class ActionsStrategiesTactics(SQLModel, table=True):
-    __tablename__ = "actions_strategies_tactics"
+class Ics201ActionsStrategiesTactics(SQLModel, table=True):
+    __tablename__ = "ics_201_actions_strategies_tactics"
 
     id: int = Field(default=None, primary_key=True)
     time_initiated: time = Field(sa_column=Field(sa_type=Time))
@@ -34,12 +34,12 @@ class ActionsStrategiesTactics(SQLModel, table=True):
     ics_201_id: Optional[int] = Field(default=None, foreign_key="ics_201.id")
 
     ics_201: Optional["Ics201"] = Relationship(
-        back_populates="actions_strategies_tactics"
+        back_populates="ics_201_actions_strategies_tactics"
     )
 
 
-class ResourceSummary(SQLModel, table=True):
-    __tablename__ = "resource_summary"
+class Ics201ResourceSummary(SQLModel, table=True):
+    __tablename__ = "ics_201_resource_summary"
 
     id: int = Field(default=None, primary_key=True)
     resource: str
@@ -51,7 +51,7 @@ class ResourceSummary(SQLModel, table=True):
     notes: str
     ics_201_id: Optional[int] = Field(default=None, foreign_key="ics_201.id")
 
-    ics_201: Optional["Ics201"] = Relationship(back_populates="resource_summary")
+    ics_201: Optional["Ics201"] = Relationship(back_populates="ics_201_resource_summary")
 
 
 class Ics201Approval(SQLModel, table=True):
@@ -64,14 +64,14 @@ class Ics201Approval(SQLModel, table=True):
     is_approved: bool
 
 
-class IcsChart(SQLModel, table=True):
-    __tablename__ = "ics_chart"
+class Ics201Chart(SQLModel, table=True):
+    __tablename__ = "ics_201_chart"
 
     id: int = Field(default=None, primary_key=True)
     ics_201_id: Optional[int] = Field(default=None, foreign_key="ics_201.id")
 
     # Relationships to Ics201
-    ics_201: Optional["Ics201"] = Relationship(back_populates="ics_chart")
+    ics_201: Optional["Ics201"] = Relationship(back_populates="ics_201_chart")
 
     # Foreign keys for main_section.py
     incident_commander_id: Optional[int] = Field(
@@ -164,87 +164,87 @@ class IcsChart(SQLModel, table=True):
 
     # Relationships to tables in main_section.py
     incident_commander: Optional["IncidentCommander"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     deputy_incident_commander: Optional["DeputyIncidentCommander"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
-    safety_officer: Optional["SafetyOfficer"] = Relationship(back_populates="ics_chart")
+    safety_officer: Optional["SafetyOfficer"] = Relationship(back_populates="ics_201_chart")
     public_information_officer: Optional["PublicInformationOfficer"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     liaison_officer: Optional["LiaisonOfficer"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
-    legal_officer: Optional["LegalOfficer"] = Relationship(back_populates="ics_chart")
+    legal_officer: Optional["LegalOfficer"] = Relationship(back_populates="ics_201_chart")
     human_capital_officer: Optional["HumanCapitalOfficer"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     operation_section_chief: Optional["OperationSectionChief"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
 
     # Relationships to tables in planning_section.py
     planning_section_chief: Optional["PlanningSectionChief"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     situation_unit_leader: Optional["SituationUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     resources_unit_leader: Optional["ResourcesUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     documentation_unit_leader: Optional["DocumentationUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     demobilization_unit_leader: Optional["DemobilizationUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     environmental_unit_leader: Optional["EnvironmentalUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     technical_specialist: Optional["TechnicalSpecialist"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
 
     # Relationships to tables in logistic_section.py
     logistic_section_chief: Optional["LogisticSectionChief"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     communication_unit_leader: Optional["CommunicationUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     medical_unit_leader: Optional["MedicalUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     food_unit_leader: Optional["FoodUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     facility_unit_leader: Optional["FacilityUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     supply_unit_leader: Optional["SupplyUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     transportation_unit_leader: Optional["TransportationUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
 
     # Relationships to tables in finance_section.py
     finance_section_chief: Optional["FinanceSectionChief"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     procurement_unit_leader: Optional["ProcurementUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     compensation_claim_unit_leader: Optional["CompensationClaimUnitLeader"] = (
-        Relationship(back_populates="ics_chart")
+        Relationship(back_populates="ics_201_chart")
     )
     cost_unit_leader: Optional["CostUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
     time_unit_leader: Optional["TimeUnitLeader"] = Relationship(
-        back_populates="ics_chart"
+        back_populates="ics_201_chart"
     )
 
 
