18705fc4eb1a7008093f04126c99fc9450b9a48e
rakaarfi <rakaarfi@gmail.com>
2025-01-30 21:47:33 +0700

    add new router

--- src/infrastructure/api/ics_202/ics_202_router.py
+++ src/infrastructure/api/ics_202/ics_202_router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
 from src.infrastructure.api.ics_202 import (
-                                ics_202, ics_202_preparation, ics_202_approval)
+                                ics_202, ics_202_preparation, ics_202_approval,
+                                ics_202_export_docx)
 
 router = APIRouter()
 
 router.include_router(ics_202.router, prefix="/main")
 router.include_router(ics_202_preparation.router, prefix="/preparation")
 router.include_router(ics_202_approval.router, prefix="/approval")
+router.include_router(ics_202_export_docx.router, prefix="/export-docx")
