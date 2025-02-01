9c3a5c9b665e10a551ac8f83ae705b05f865511b
rakaarfi <rakaarfi@gmail.com>
2025-02-01 09:27:51 +0700

    add new routes

---
+++ src/infrastructure/api/ics_204/ics_204_preparation_ru_leader.py
@@ -0,0 +1,87 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+
+from src.infrastructure.config.database import get_session
+from src.infrastructure.database.repositories.base_repository import BaseRepository
+from src.core.entities.ics_204_models import Ics204PreparationRULeader
+from src.core.entities.pagination_models import PaginationResponse
+from src.core.exceptions import NotFoundException, BadRequestException
+
+
+router = APIRouter()
+
+# Dependency to get the repository
+def get_repository(session: AsyncSession = Depends(get_session)) -> BaseRepository:
+    return BaseRepository(session)
+
+
+# Endpoint to create
+@router.post("/create/", summary="Create Ics 204 Preparation RU Leader", description="Create a new Ics 204 Preparation RU Leader")
+async def create_ics_204_preparation_ru_leader(
+    item: Ics204PreparationRULeader, repo: BaseRepository = Depends(get_repository)
+):
+    return await repo.create_item(Ics204PreparationRULeader, item)
+
+
+# Endpoint to read all
+@router.get("/read/")
+async def read_ics_204_preparation_ru_leader(repo: BaseRepository = Depends(get_repository)):
+    return await repo.read_items(Ics204PreparationRULeader)
+
+
+# Endpoint to read by id
+@router.get("/read/{id}")
+async def read_ics_204_preparation_ru_leader_by_id(
+    id: int, repo: BaseRepository = Depends(get_repository)
+):
+    try:
+        return await repo.read_item_by_id(Ics204PreparationRULeader, id)
+    except NotFoundException as e:
+        raise e
+    
+
+# Endpoint to update
+@router.put("/update/{id}")
+async def update_ics_204_preparation_ru_leader(
+    updated_data: Ics204PreparationRULeader, id: int, repo: BaseRepository = Depends(get_repository)
+):
+    try:
+        return await repo.update_item(Ics204PreparationRULeader, id, updated_data)
+    except NotFoundException as e:
+        raise e
+
+
+# Endpoint to delete
+@router.delete("/delete/{id}")
+async def delete_ics_204_preparation_ru_leader(
+    id: int, repo: BaseRepository = Depends(get_repository)
+):
+    try:
+        return await repo.delete_item(Ics204PreparationRULeader, id)
+    except NotFoundException as e:
+        raise e
+    
+    
+# Endpoint to read all with pagination
+@router.get("/read-paginated/", response_model=PaginationResponse[List[Ics204PreparationRULeader]])
+async def read_ics_204_preparation_ru_leader_paginated(
+    page: int = 1,
+    limit: int = 10,
+    search: str = "",
+    repo: BaseRepository = Depends(get_repository),
+):
+    condition = None
+
+    return await repo.read_paginated_items(Ics204PreparationRULeader, page, limit, condition)
+
+
+# Endpoint untuk read by ics_204_id
+@router.get("/read-by-ics-204-id/{ics_204_id}")
+async def read_ics_204_preparation_ru_leader_by_ics_204_id(
+    ics_204_id: int, repo: BaseRepository = Depends(get_repository)
+):
+    condition = Ics204PreparationRULeader.ics_204_id == ics_204_id
+    return await repo.read_items_by_condition(Ics204PreparationRULeader, condition)
