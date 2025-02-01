a2763d964b357c5997478cb6f1d620bfeaf01aa1
rakaarfi <rakaarfi@gmail.com>
2025-02-01 09:27:00 +0700

    add router

--- src/infrastructure/api/ics_204/ics_204_router.py
+++ src/infrastructure/api/ics_204/ics_204_router.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
 from src.infrastructure.api.ics_204 import (
-                                ics_204, ics_204_preparation,
-                                personnel_assigned, equipment_assigned)
+                                ics_204, personnel_assigned, equipment_assigned,
+                                ics_204_preparation_os_chief, ics_204_preparation_ru_leader
+                                )
 
 router = APIRouter()
 
 router.include_router(ics_204.router, prefix="/main")
 router.include_router(personnel_assigned.router, prefix="/personnel-assigned")
 router.include_router(equipment_assigned.router, prefix="/equipment-assigned")
-router.include_router(ics_204_preparation.router, prefix="/preparation")
+router.include_router(ics_204_preparation_os_chief.router, prefix="/preparation-os-chief")
+router.include_router(ics_204_preparation_ru_leader.router, prefix="/preparation-ru-leader")
