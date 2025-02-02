2a09f9a896e9b187ec907c8a928c027043da3c06
rakaarfi <rakaarfi@gmail.com>
2025-02-02 22:08:12 +0700

    add export router

--- src/infrastructure/api/ics_208/ics_208_router.py
+++ src/infrastructure/api/ics_208/ics_208_router.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 
 from src.infrastructure.api.ics_208 import (
-                                ics_208, ics_208_preparation)
+                                ics_208, ics_208_preparation,
+                                ics_208_export_docx
+                                )
 
 router = APIRouter()
 
 router.include_router(ics_208.router, prefix="/main")
 router.include_router(ics_208_preparation.router, prefix="/preparation")
+router.include_router(ics_208_export_docx.router, prefix="/export-docx")
