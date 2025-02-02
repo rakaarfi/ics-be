16283b326dd7af8e4d8ef5d3c47189fd0ac6b1c9
rakaarfi <rakaarfi@gmail.com>
2025-02-02 22:07:07 +0700

    add export router

--- src/infrastructure/api/ics_206/ics_206_router.py
+++ src/infrastructure/api/ics_206/ics_206_router.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter
 from src.infrastructure.api.ics_206 import (
                                 ics_206, ics_206_preparation,
                                 hospitals, medical_aid_station,
-                                transportation, ics_206_approval)
+                                transportation, ics_206_approval,
+                                ics_206_export_docx)
 
 router = APIRouter()
 
@@ -13,3 +14,4 @@ router.include_router(medical_aid_station.router, prefix="/medical-aid-station")
 router.include_router(transportation.router, prefix="/transportation")
 router.include_router(ics_206_preparation.router, prefix="/preparation")
 router.include_router(ics_206_approval.router, prefix="/approval")
+router.include_router(ics_206_export_docx.router, prefix="/export-docx")
