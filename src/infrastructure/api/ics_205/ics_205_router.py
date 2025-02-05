eb134487e9d2979669d091aa93abe6b2b214c7fc
rakaarfi <rakaarfi@gmail.com>
2025-02-05 23:41:28 +0700

    add new route

--- src/infrastructure/api/ics_205/ics_205_router.py
+++ src/infrastructure/api/ics_205/ics_205_router.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 
 from src.infrastructure.api.ics_205 import (
                                 ics_205, ics_205_preparation,
-                                radio_channel)
+                                radio_channel, ics_205_export_docx)
 
 router = APIRouter()
 
 router.include_router(ics_205.router, prefix="/main")
 router.include_router(radio_channel.router, prefix="/radio-channel")
 router.include_router(ics_205_preparation.router, prefix="/preparation")
+router.include_router(ics_205_export_docx.router, prefix="/export-docx")
